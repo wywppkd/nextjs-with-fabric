17e8de265aab3d7fe8c35bc55b84e299e72581f6
Bran <1055025755@qq.com>
2022-01-14 18:35:17 +0800

    feat: lint-fix 格式化范围

---
+++ components/Apple.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Apple = () => {
+  return <div>hello apple</div>;
+};
+
+export default Apple;
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
 import styles from './index.module.scss';
 
 const About = () => {
-  return <div className={styles.about}>about</div>;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('🚀 ~ file: index.tsx ~ line 9 ~ useEffect ~ count', count);
+  }, [count]);
+
+  return (
+    <div className={styles.about}>
+      <div
+        onClick={() => {
+          setCount(Date.now());
+        }}
+      >
+        count:{count}
+      </div>
+    </div>
+  );
 };
 
 export default About;
