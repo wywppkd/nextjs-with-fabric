6e440739c633c4fdce5399e9eb2943f26f7b982b
Bran <1055025755@qq.com>
2022-01-17 10:39:45 +0800

    eslint: config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,6 @@ module.exports = {
     'no-console': 'off',
     'react-hooks/exhaustive-deps': 'error', // react-hooks 依赖检查
     'no-empty': 'off', // catch{} 允许为空
+    '@typescript-eslint/no-shadow': ['off'], // 当前作用域变量名不能与父级作用域变量同名
   },
 };
